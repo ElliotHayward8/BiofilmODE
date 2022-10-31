2763fa793164469e372d54b9fcb24c19a031c3e3
ElliotHayward8 <vu19320@bristol.ac.uk>
2022-10-31 16:11:30 +0000

    Add treatment phase plotted on same graph

--- BiofilmODE.py
+++ BiofilmODE.py
@@ -34,7 +34,7 @@ def grow_phase(N, t, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A):
 
     # Calculate a and b depending on the concentration of antibiotic and substrate
     a = (a_max * (1 - (CS/(CS + K1)))) + (a_max * (CA/(CA + K2)))
-    b = 0.5 * (0.1 * b_max * (CS/(CS + K1))) + (b_max * (1 - (CA/(CA + K2))))  # 0.1 to match the default NetLogo model
+    b = 0.5 * (0.1 * b_max * (CS/(CS + K1))) + (b_max * (1 - (CA/(CA + K2))))  # 0.1 is to match the default model
     dNsdt = G*Ns + b*Np - a*Ns - kmaxs * CA/(CA + K_A)  # ODE for the rate of change of susceptible cells
     dNpdt = a*Ns - b*Np - kmaxp * CA/(CA + K_A)  # ODE for the rate of change of persister cells
 
@@ -47,20 +47,35 @@ def main():
     """
     Main function where parameter values are defined and the process is run
     """
+    grow_hours = 8  # Define the number of hours for the biofilm to grow
+    treat_hours = 2  # Define the number of hours the biofilm is treated for
+    MIC = 0.000005  # Minimal Inhibitory Concentration of the antibiotic
+
+    CA = 1000 * MIC  # Define the concentration of antibiotic present during the treatment phase
     Ns0, Np0 = 10, 0  # Initial number of each type of cell
     N = [Ns0, Np0]
     CS0 = 0.4  # Bulk concentration of substrate
-    G = 1.25  # Growing rate of the bacteria (Need to equate to mu_max)
+    G = 0.75  # Growing rate of the bacteria (Need to equate to mu_max)
     K1, K2 = 0.0035, 0.000005  # Half saturation rate for the substrate and antibiotic respectively
-    t = np.linspace(0, 8, 36000)  # Define the time range to solve over
+
+    t_grow, t_treat = np.linspace(0, grow_hours, grow_hours*3600), np.linspace(0, treat_hours, treat_hours*3600)
+
     a_max, b_max = 1, 1
     kmaxs, kmaxp = 10, 0.1  # Define the killing rates of both types of bacteria
     K_A = 6.4  # Define the half-saturation constant for the killing rates of susceptible and persister cells
 
-    sol = odeint(grow_phase, N, t, args=(0, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A))
+    sol = odeint(grow_phase, N, t_grow, args=(0, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A))
 
-    plt.plot(t, sol[:, 0], 'b', label='Ns')
-    plt.plot(t, sol[:, 1], 'g', label='Np')
+    final_N = [sol[-1, 0], sol[-1, 1]]
+
+    sol2 = odeint(grow_phase, final_N, t_treat, args=(CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A))
+
+    # Combine the solutions to produce one graph
+    full_sol = np.concatenate((sol, sol2))
+    t_total = np.concatenate((t_grow, t_treat + grow_hours))
+
+    plt.plot(t_total, full_sol[:, 0], 'b', label='Ns')
+    plt.plot(t_total, full_sol[:, 1], 'g', label='Np')
     plt.legend(loc='best')
     plt.xlabel('t')
     plt.show()
