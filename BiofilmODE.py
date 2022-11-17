dd4d2992c8b40dda69e46a0891ca31e09c804013
ElliotHayward8 <vu19320@bristol.ac.uk>
2022-11-17 13:34:16 +0000

    Add PlotHeatmapfromCSV.py to plot the Heatmap off the back of the CSV file

--- BiofilmODE.py
+++ BiofilmODE.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy.integrate import odeint
+import csv
 
 """
 Notes:
@@ -12,7 +13,7 @@ maximum value for these, choose where to define this value - could change betwee
 """
 
 
-def grow_phase(N, t, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, switch_type):
+def grow_phase(N, t, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S):
     """
     Function of ODEs to model the growth phase of the biofilm, measuring the number of susceptible
     and percepter cells
@@ -40,9 +41,10 @@ def grow_phase(N, t, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, s
     G = G * (CS / (CS + K_S))
 
     # Calculate a and b depending on the concentration of antibiotic and substrate
-    if switch_type == 1:  # Combination dependent switching
-        a = (a_max * (1 - (CS / (CS + K1)))) + (a_max * (CA / (CA + K2)))
-        b = 0.5 * (b_max * (CS / (CS + K1))) + (b_max * (1 - (CA / (CA + K2))))
+
+    a = (a_max * (1 - (CS / (CS + K1)))) + (a_max * (CA / (CA + K2)))
+    b = 0.5 * (b_max * (CS / (CS + K1))) + (b_max * (1 - (CA / (CA + K2))))
+    """
     elif switch_type == 2:  # Substrate dependent switching
         a = a_max * (1 - (CS / (CS + K1)))
         b = 0.1 * b_max * (CS / (CS + K1))
@@ -51,7 +53,7 @@ def grow_phase(N, t, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, s
         b = b_max * (1 - (CA / (CA + K2)))
     else:
         raise ValueError('switch_type must be 1, 2 or 3')
-
+    """
     dNsdt = G * Ns + b * Np - a * Ns - Ns * (kmaxs * (CA / (CA + K_A)))  # Rate of change of susceptible cells
     dNpdt = a * Ns - b * Np - Np * (kmaxp * (CA / (CA + K_A)))  # Rate of change of persister cells
 
@@ -61,7 +63,7 @@ def grow_phase(N, t, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, s
     return [dNsdt, dNpdt]
 
 
-def run_biofilm(N, T, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, switch_type):
+def run_biofilm(N, T, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S):
     """
     Function to run the ODEs for time T with the inputted parameters
     :param N: Number of persister and susceptible cells
@@ -80,12 +82,12 @@ def run_biofilm(N, T, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, 
     """
     t_run = np.linspace(0, T, T * 3600)
 
-    sol = odeint(grow_phase, N, t_run, args=(CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, switch_type))
+    sol = odeint(grow_phase, N, t_run, args=(CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S))
 
     return sol
 
 
-def constant_treatment(N, T_grow, T1, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, switch_type):
+def constant_treatment(N, T_grow, T1, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S):
     """
     A biofilm treatment strategy where the antibiotic is applied constantly
     :param T_grow: The time the biofilm grows before it is first treated
@@ -95,7 +97,7 @@ def constant_treatment(N, T_grow, T1, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, k
     t_grow = np.linspace(0, T_grow, int(3600 * T_grow))
     t_treat = np.linspace(0, T1, int(3600 * T1))
 
-    sol = odeint(grow_phase, N, t_grow, args=(0, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, switch_type))
+    sol = odeint(grow_phase, N, t_grow, args=(0, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S))
     full_sol, t_total = sol, t_grow
     final_N = [full_sol[-1, 0], full_sol[-1, 1]]
 
@@ -103,8 +105,7 @@ def constant_treatment(N, T_grow, T1, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, k
     check = 0
     while sum(final_N) > end_val and total_time < 250:
         treat_sol = odeint(grow_phase, final_N, t_treat, args=(CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp,
-                                                               K_A, K_S, switch_type))
-
+                                                               K_A, K_S))
         final_N = [treat_sol[-1, 0], treat_sol[-1, 1]]
         for i in range(len(treat_sol)):
             if check == 0:
@@ -117,7 +118,7 @@ def constant_treatment(N, T_grow, T1, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, k
     return full_sol, t_total, end_time
 
 
-def constant_switch(N, T_grow, T1, T2, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, switch_type):
+def constant_switch(N, T_grow, T1, T2, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S):
     """
     A biofilm treatment strategy where the antibiotic is applied and removed for constant time periods (keep T">0.1)
     :param T_grow: The time the biofilm grows before it is first treated
@@ -127,14 +128,14 @@ def constant_switch(N, T_grow, T1, T2, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, 
     end_val, t_grow, t_treat = 0.5, np.linspace(0, T_grow, int(3600 * T_grow)), np.linspace(0, T1, int(3600 * T1))
     t_regrow = np.linspace(0, T2, int(3600 * T2))
 
-    sol = odeint(grow_phase, N, t_grow, args=(0, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, switch_type))
+    sol = odeint(grow_phase, N, t_grow, args=(0, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S))
     full_sol, t_total = sol, t_grow
     final_N = [full_sol[-1, 0], full_sol[-1, 1]]
 
     total_time, check = T_grow, 0
     while sum(final_N) > end_val and total_time < 250:
         treat_sol = odeint(grow_phase, final_N, t_treat, args=(CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp,
-                                                               K_A, K_S, switch_type))
+                                                               K_A, K_S))
         final_N = [treat_sol[-1, 0], treat_sol[-1, 1]]
         for i in range(len(treat_sol)):
             if check == 0:
@@ -146,7 +147,7 @@ def constant_switch(N, T_grow, T1, T2, CA, CS0, a_max, b_max, K1, K2, G, kmaxs, 
 
         total_time = total_time + T1
         regrow_sol = odeint(grow_phase, final_N, t_regrow, args=(0, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp,
-                                                                 K_A, K_S, switch_type))
+                                                                 K_A, K_S))
 
         final_N = [regrow_sol[-1, 0], regrow_sol[-1, 1]]
         if check == 0:
@@ -178,7 +179,7 @@ def param_scan(N, T_grow, T1s, T2s, CA, CS0, a_maxs, b_maxs, K1, K2, G, kmaxs, k
             for T1 in T1s:
                 for T2 in T2s:
                     full_sol, t_total, end_time = constant_switch(N, T_grow, T1, T2, CA, CS0, a_max, b_max, K1, K2, G,
-                                                                  kmaxs, kmaxp, K_A, K_S, 1)
+                                                                  kmaxs, kmaxp, K_A, K_S)
 
                     if T1_results:
                         a_max_results.append(a_max), b_max_results.append(b_max), T1_results.append(T1)
@@ -205,14 +206,21 @@ def best_param_scan(N, T_grow, T1s, T2s, CA, CS0, a_maxs, b_maxs, K1, K2, G, kma
         for b_max in b_maxs:
             print('a = ' + str(a_max) + '        b = ' + str(b_max))  # For tracking the progress of the scan
             a_max_vals.append(a_max), b_max_vals.append(b_max)
-            best_T1.append(0), best_T2.append(0), best_end_time.append(5000000)
+            best_T1.append(0), best_T2.append(0), best_end_time.append(500)
             for T1 in T1s:
                 for T2 in T2s:
                     full_sol, t_total, end_time = constant_switch(N, T_grow, T1, T2, CA, CS0, a_max, b_max, K1, K2, G,
-                                                                  kmaxs, kmaxp, K_A, K_S, 1)
+                                                                  kmaxs, kmaxp, K_A, K_S)
 
                     if end_time < best_end_time[-1]:
                         best_end_time[-1], best_T1[-1], best_T2[-1] = end_time, T1, T2
+
+    fieldnames = ['a_max', 'b_max', 'T1', 'T2', 't_total']
+    with open('output.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(i for i in fieldnames)
+        for i in range(len(a_max_vals)):
+            writer.writerow([a_max_vals[i], b_max_vals[i], best_T1[i], best_T2[i], best_end_time[i]])
 
     return a_max_vals, b_max_vals, best_T1, best_T2, best_end_time
 
@@ -231,7 +239,6 @@ def main():
     """
     G = 1.278  # Growing rate of the bacteria (Need to equate to mu_max), include the substrate concentration
     K1, K2 = 0.0035, 0.000005  # Half saturation constant for the substrate and antibiotic respectively
-    switch = 1  # Define the switching type (1 = combo, 2 = substrate, 3 = antibiotic)
     a_max, b_max = 1, 1  # Typical values - (1, 1)
     kmaxs, kmaxp = 10, 0.1  # Define the killing rates of both types of bacteria (10, 0.1)
     K_A = 6.4 * MIC  # Define the half-saturation constant for the killing rates of susceptible and persister cells
@@ -242,13 +249,13 @@ def main():
     t_grow, t_treat = np.linspace(0, grow_hours, grow_hours * 3600), np.linspace(0, treat_hours, treat_hours * 3600)
     t_recov = np.linspace(0, recov_hours, recov_hours * 3600)
     
-    sol = odeint(grow_phase, N, t_grow, args=(0, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, switch))
+    sol = odeint(grow_phase, N, t_grow, args=(0, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S))
     final_N = [sol[-1, 0], sol[-1, 1]]
 
-    sol2 = odeint(grow_phase, final_N, t_treat, args=(CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, switch))
+    sol2 = odeint(grow_phase, final_N, t_treat, args=(CA, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S))
     final_N = [sol2[-1, 0], sol2[-1, 1]]
 
-    sol3 = odeint(grow_phase, final_N, t_recov, args=(0, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S, switch))
+    sol3 = odeint(grow_phase, final_N, t_recov, args=(0, CS0, a_max, b_max, K1, K2, G, kmaxs, kmaxp, K_A, K_S))
 
     # Combine the solutions to produce one graph
     full_sol = np.concatenate((sol, sol2, sol3))
@@ -263,47 +270,48 @@ def main():
     # Define parameters for the parameter scan
     # a_maxs = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     # b_maxs = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    a_s, b_s = np.linspace(0.1, 1.0, 12), np.linspace(0.1, 1.0, 12)
+    a_s, b_s = np.linspace(0.1, 1.0, 5), np.linspace(1.0, 0.1, 5)
     # T1s = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     # T2s = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    T1s, T2s = np.linspace(1/12, 1, 6), np.linspace(1/12, 1.5, 6)
+    T1s, T2s = np.linspace(1/12, 1, 4), np.linspace(1/12, 2, 4)
 
     """
     a_max_results, b_max_results, T1_results, T2_results, end_time_results = param_scan(N, 5, T1s, T2s, CA, CS0, a_s,
                                                                                         b_s, K1, K2, G, kmaxs, kmaxp,
                                                                                         K_A, K_S)
     """
-    print(np.arange(len(a_s))-0.5)
+
     a_max_results, b_max_results, T1_results, T2_results, end_time_results = best_param_scan(N, 5, T1s, T2s, CA, CS0,
                                                                                              a_s, b_s, K1, K2, G,
                                                                                              kmaxs, kmaxp, K_A, K_S)
 
     T1_results, T2_results = np.asarray(T1_results), np.asarray(T2_results)
-    T1_results, T2_results = np.reshape(T1_results, (len(b_s), len(a_s))), np.reshape(T2_results, (len(b_s), len(a_s)))
+    T1_results, T2_results = np.reshape(T1_results, (len(b_s), len(a_s))).T, np.reshape(T2_results,
+                                                                                        (len(b_s), len(a_s))).T
 
     fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(8, 4))
-    tk, x0 = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0], -0.5
+    tkx, x0 = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0], -0.5
+    tky = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
     x_len, y_len = len(a_s) + x0, len(b_s) + x0
     im1, im2 = ax1.imshow(T1_results*60, cmap='viridis'), ax2.imshow(T2_results*60, cmap='viridis')
 
-    ax1.set_xticks(np.linspace(x0, x_len, 10), labels=tk), ax2.set_xticks(np.linspace(x0, x_len, 10), labels=tk)
-    ax1.set_yticks(np.linspace(x0, y_len, 10), labels=tk), ax2.set_yticks(np.linspace(x0, y_len, 10), labels=tk)
+    ax1.set_xticks(np.linspace(x0, x_len, 10), labels=tkx), ax2.set_xticks(np.linspace(x0, x_len, 10), labels=tkx)
+    ax1.set_yticks(np.linspace(x0, y_len, 10), labels=tky), ax2.set_yticks(np.linspace(x0, y_len, 10), labels=tky)
     ax1.set_xticks(np.arange(len(a_s))-0.5, minor=True), ax2.set_xticks(np.arange(len(a_s))-0.5, minor=True)
     ax1.set_yticks(np.arange(len(b_s))-0.5, minor=True), ax2.set_yticks(np.arange(len(b_s))-0.5, minor=True)
-    ax1.grid(which='minor', color='k', linestyle='-', linewidth=0.7), ax2.grid(which='minor', color='k', linestyle='-',
-                                                                               linewidth=0.7)
+    ax1.grid(which='minor', color='k', linestyle='-', linewidth=0.3), ax2.grid(which='minor', color='k', linestyle='-',
+                                                                               linewidth=0.3)
     ax1.tick_params(axis='both', which='both', length=0), ax2.tick_params(axis='both', which='both', length=0)
 
     divider1, divider2 = make_axes_locatable(ax1), make_axes_locatable(ax2)
     cax1, cax2 = divider1.append_axes("right", size="5%", pad=0.2), divider2.append_axes("right", size="5%", pad=0.5)
     cbar1, cbar2 = plt.colorbar(im1, cax=cax1), plt.colorbar(im2, cax=cax2)
-    cbar1.set_label(label='Optimised treatment duration (min)', size=17)
-    cbar2.set_label(label='Optimised treatment duration (min)', size=17)
+    cbar1.set_label(label='Optimised treatment duration (min)', size=10)
+    cbar2.set_label(label='Optimised treatment duration (min)', size=10)
     ax1.set_xlabel('$a_{max}$'), ax2.set_xlabel('$a_{max}$'), ax1.set_ylabel('$b_{max}$'), ax2.set_ylabel('$b_{max}$')
-    plt.subplots_adjust(wspace=0.1)
-    plt.savefig("ParameterScan.pdf", bbox_inches='tight', pad_inches=0)
+    fig.tight_layout(pad=5)
+    # plt.savefig("ParameterScan.pdf", bbox_inches='tight', pad_inches=0)
     plt.show()
-
 
     # full_sol, t_total, end_time = constant_switch(N, 5, 0.5, 0.5, CA, CS0, a_max, b_max, K1, K2, G, kmaxs,
     #                                               kmaxp, K_A, K_S, switch)
---
+++ PlotHeatmapfromCSV.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from scipy.integrate import odeint
+import csv
+
+
+def main():
+    """
+    Main function where parameter values are defined and the process is run
+    """
+
+    a_max_results, b_max_results, T1_results, T2_results, end_time_results = [], [], [], [], []
+
+    file = open('output.csv')
+    csvreader = csv.reader(file)
+    header = []
+    header = next(csvreader)
+
+    rows = []
+    for row in csvreader:
+        rows.append(row)
+
+    for row in rows:
+        a_max_results.append(float(row[0])), b_max_results.append(float(row[1])), T1_results.append(float(row[2]))
+        T2_results.append(float(row[3])), end_time_results.append(float(row[4]))
+
+    a_s, b_s = set(a_max_results), set(b_max_results)
+
+    T1_results, T2_results = np.asarray(T1_results), np.asarray(T2_results)
+    T1_results, T2_results = np.reshape(T1_results, (len(b_s), len(a_s))).T, np.reshape(T2_results,
+                                                                                        (len(b_s), len(a_s))).T
+
+    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(8, 4))
+    tkx, x0 = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0], -0.5
+    tky = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
+    x_len, y_len = len(a_s) + x0, len(b_s) + x0
+
+    im1, im2 = ax1.imshow(T1_results*60, cmap='viridis'), ax2.imshow(T2_results*60, cmap='viridis')
+
+    ax1.set_xticks(np.linspace(x0, x_len, 10), labels=tkx), ax2.set_xticks(np.linspace(x0, x_len, 10), labels=tkx)
+    ax1.set_yticks(np.linspace(x0, y_len, 10), labels=tky), ax2.set_yticks(np.linspace(x0, y_len, 10), labels=tky)
+    # Set ticks to surround each plotted box
+    ax1.set_xticks(np.arange(len(a_s))-0.5, minor=True), ax2.set_xticks(np.arange(len(a_s))-0.5, minor=True)
+    ax1.set_yticks(np.arange(len(b_s))-0.5, minor=True), ax2.set_yticks(np.arange(len(b_s))-0.5, minor=True)
+    # Plot a box around each value
+    ax1.grid(which='minor', color='k', linestyle='-', linewidth=0.3), ax2.grid(which='minor', color='k', linestyle='-',
+                                                                               linewidth=0.3)
+    ax1.tick_params(axis='both', which='both', length=0), ax2.tick_params(axis='both', which='both', length=0)
+
+    divider1, divider2 = make_axes_locatable(ax1), make_axes_locatable(ax2)
+    cax1, cax2 = divider1.append_axes("right", size="5%", pad=0.2), divider2.append_axes("right", size="5%", pad=0.5)
+    cbar1, cbar2 = plt.colorbar(im1, cax=cax1), plt.colorbar(im2, cax=cax2)
+    cbar1.set_label(label='Optimised treatment duration (min)', size=10)
+    cbar2.set_label(label='Optimised treatment duration (min)', size=10)
+    ax1.set_xlabel('$a_{max}$'), ax2.set_xlabel('$a_{max}$'), ax1.set_ylabel('$b_{max}$'), ax2.set_ylabel('$b_{max}$')
+    fig.tight_layout(pad=5)
+    plt.savefig("ParameterScanTrial.pdf", bbox_inches='tight', pad_inches=0)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
